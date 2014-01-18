728a1fdb4e7c52c1a36cb841b7dca52ee3245a6a
Daniel Alberto <tamarindodn@hotmail.com>
2014-01-18 17:04:22 -0500

    maquetacion y recorte de img - 18/01/2014

--- tamarindo_labs/urls.py
+++ tamarindo_labs/urls.py
@@ -5,10 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tamarindo_website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(website_urls)),
+    url(r'^', include(website_urls))
 )
