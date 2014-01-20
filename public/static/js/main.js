f588ff0304fdbfc4f2751e1a5f3be18a0a8fe7b2
Daniel Alberto <tamarindodn@hotmail.com>
2014-01-20 10:13:58 -0500

    maquetacion servivios 55%

---
+++ public/static/js/main.js
@@ -0,0 +1,23 @@
+$('#panelDisenoDesarrollo .panelvisible .iconoabp'). on('mouseenter', function() {
+	$('#panelDisenoDesarrollo .panelinvisible ').show("slow");
+    });
+
+$('#panelDisenoDesarrollo .panelinvisible .bnt_retractil'). on('click', function() {
+	$('#panelDisenoDesarrollo .panelinvisible ').hide("slow");
+    });
+
+$('#panelAdminwebsite .panelvisible .iconoabp'). on('mouseenter', function() {
+	$('#panelAdminwebsite .panelinvisible ').show("slow");
+    });
+
+$('#panelAdminwebsite .panelinvisible .bnt_retractil'). on('click', function() {
+	$('#panelAdminwebsite .panelinvisible ').hide("slow");
+    });
+
+$('#panelAppmoviles .panelvisible .iconoabp'). on('mouseenter', function() {
+	$('#panelAppmoviles .panelinvisible ').show("slow");
+    });
+
+$('#panelAppmoviles .panelinvisible .bnt_retractil'). on('click', function() {
+	$('#panelAppmoviles .panelinvisible ').hide("slow");
+    });
