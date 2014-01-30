0bfbb86fea3ba4fbfdc9d0e10770f7094b348dd9
Daniel Alberto <tamarindodn@hotmail.com>
2014-01-30 01:45:58 -0500

    database admin_sa

--- apps/admin_sas/models.py
+++ apps/admin_sas/models.py
@@ -23,13 +23,31 @@ class GenericManager(models.Manager):
         except self.model.MultipleObjectsReturned:
             return None
 
-class Service(models.Model):
-    """Extended profile to Django User model"""
-    nombre = models.CharField(max_length=30, verbose_name=_("nombre"))
-    precio = models.IntegerField(max_length=20,verbose_name=_("Precio"))   
+
+
+class type_service(models.Model):
+    name = models.CharField(max_length=30, verbose_name=_("nombre"))
     description = models.CharField(max_length=900,null=True,blank=True,verbose_name=_("descripcion"))
-  	# Requerimientos
-    type_service =	models.CharField(max_length=250,verbose_name=_("Valor"))
+    is_sas = models.BooleanField()
+    own_development = models.BooleanField()
+    provider = models.CharField(max_length=30, verbose_name=_("provedor"))
+    # ------ datos para todas las tablas
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    objects = GenericManager()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=30, verbose_name=_("nombre"))
+    price = models.IntegerField(max_length=20,verbose_name=_("Precio"))   
+    description = models.CharField(max_length=900,null=True,blank=True,verbose_name=_("descripcion"))
+    # Requerimientos
+    type_service =  models.CharField(max_length=250,verbose_name=_("Valor"))
     time_license  = models.IntegerField(max_length=20,verbose_name=_("tiempo licencia")) 
     developed_by = models.CharField(max_length=250,null=True,blank=True,verbose_name=_("descripcion"))
     # ------ datos para todas las tablas
@@ -40,16 +58,14 @@ class Service(models.Model):
     objects = GenericManager()
 
     def __unicode__(self):
-        return self.short_name
+        return self.name
 
 
 class Company(models.Model):
-    """Extended profile to Django User model"""
-    
     Nit = models.CharField(max_length=30, null=False, verbose_name=_("NIT"))
     legal_forma = models.CharField(max_length=30, verbose_name=_("Rorma Legal"), null=True, blank=True)
-    razon = models.CharField(max_length=30, verbose_name=_("Razon"), null=True, blank=True)
-    ambito = models.CharField(max_length=30, verbose_name=_("Ambito"), null=True, blank=True)
+    name = models.CharField(max_length=30, verbose_name=_("Razon"), null=True, blank=True)
+    scope = models.CharField(max_length=30, verbose_name=_("Ambito"), null=True, blank=True)
     phone1 = models.CharField(max_length=30, verbose_name=_("Telefono 1"), null=True, blank=True)
     phone2 = models.CharField(max_length=30, verbose_name=_("Telefono 2"), null=True, blank=True)
     address = models.CharField(max_length=150, verbose_name=_("Direccion"), null=True, blank=True)    
@@ -58,8 +74,7 @@ class Company(models.Model):
     country = models.CharField(max_length=60, verbose_name=_("Pais"), null=True, blank=True)
     commercial_activity = models.CharField(max_length=60, verbose_name=_("Pais"), null=True, blank=True)     
     logo = models.FileField(upload_to=settings.MEDIA_ROOT + "/logos", max_length=400, null=True, blank=True)
-
-    service =  models.OneToOneField(Service, verbose_name=_("Service"))
+    service = models.ForeignKey(Service, verbose_name=_("Service"))
     # ------ datos para todas las tablas
     is_active = models.BooleanField(default=True)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -67,14 +82,50 @@ class Company(models.Model):
     objects = GenericManager()
 
     def __unicode__(self):
-        return self.short_name
+        return self.name
+
+
+class implementation(models.Model):
+    service = models.ForeignKey(Service)
+    company = models.ForeignKey(Company)
+    name = models.CharField(max_length=30, verbose_name=_("nombre"))
+    #discounts = models.BooleanField() falta implementacion
+    release_date = models.DateTimeField(auto_now_add=True)
+    version =  models.CharField(max_length=30, verbose_name=_("provedor"))
+    #additional_service =  models.ManyToManyField(Service) 
+    # ------ datos para todas las tablas
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    objects = GenericManager()
+
+    def __unicode__(self):
+        return self.name
+
+
+class image_portfolio(models.Model):
+    name = models.CharField(max_length=30, verbose_name=_("nombre")) 
+    descripcion =  models.CharField(max_length=900,null=True,blank=True,verbose_name=_("descripcion"))
+    img = models.FileField(upload_to=settings.MEDIA_ROOT + "/img", max_length=400, null=True, blank=True)
+    order = models.IntegerField(max_length=20,verbose_name=_("orden"))
+    service = models.ForeignKey(Service)
+    # ------ datos para todas las tablas
+    is_active = models.BooleanField(default=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    objects = GenericManager()
+
+    def __unicode__(self):
+        return self.name
+
 
 class DniType(models.Model):
     """List of DNI types"""
     short_name = models.CharField(max_length=40, verbose_name=_("Nombre corto"))
     long_name = models.CharField(max_length=300, verbose_name=_("Nombre Largo"))
     # ------ datos para todas las tablas
-
     is_active = models.BooleanField(default=True)
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
@@ -82,8 +133,6 @@ class DniType(models.Model):
 
     def __unicode__(self):
         return self.short_name
-
-
 
 
 class UserProfile(models.Model):
@@ -107,7 +156,7 @@ class UserProfile(models.Model):
     objects = GenericManager()
 
     def __unicode__(self):
-        return self.short_name
+        return self.user.username
 
 
 
